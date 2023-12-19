9f9d64db6df8066ca2e3172220e606334ec71732
william.li <william.li@ll.mit.edu>
2023-12-19 08:26:00 -0500

    Merging updates from VsCodex [x] Getting the latest updates from the codex

--- ci/scripts/run_coverage.py
+++
@@ -1,7 +0,0 @@
-"""This file should be placed in your ./src directory.  When ready to run coverage simply run this script."""
-from os import system
-
-# Run coverage using pytest, then record results to docs.
-system("coverage run -m pytest")
-system("coverage html -d 'save/coverage'")
-system("coverage report -m")
